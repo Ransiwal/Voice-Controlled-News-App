aee1f03042db259ee1f4fb0a227c8727bd1c9f2a
Suryansh <[ransiwalsuryansh@gmail.com]>
2024-06-25 17:28:20 +0530

    alan studio code is added in ALAN.txt file

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
 
   useEffect(() => {
     alanBtn({
-      key: '5ff66fbb6820897c3311138cf6c501192e956eca572e1d8b807a3e2338fdd0dc/stage',
+      key: '5ff66fbb6820897c3311138cf6a3e2338fdd0dc/stage',
       onCommand: ({ command, articles, number }) => {
         if (command === 'newHeadlines') {
           setNewsArticles(articles);
